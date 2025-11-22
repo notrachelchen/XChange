982cdeeca2894853cbe9c12b82a3a3eab9d9c9a0
notrachelchen <chenrachel22@gmail.com>
2025-11-21 22:14:31 -0500

    Initial Commit

---
+++ XChange/XChangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XChangeApp.swift
+//  XChange
+//
+//  Created by Rachel Chen on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct XChangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
